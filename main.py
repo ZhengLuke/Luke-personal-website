81666686114509e9472c91f0e6e60f0ca79b9773
Luke Zheng <zheng.huguang.luke@gmail.com>
2018-10-15 21:37:00 -0400

    some fix

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-## This is the file with the main functions for the website backend
+import sys, os
+from flask import Flask, render_template, request
+
+import json
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
